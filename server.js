62443442b9d6ab1c8b6a6a3c3a759f3712e4ac11
Buddhika Madusanka <buddhikadevelopment@gmail.com>
2025-03-07 10:03:04 +0530

    Setup basic server and database connection

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const db_connection = require('./utils/db')
+
+dotenv.config()
+
+// configure basic app
 const app = express()
-const PORT = 8080;
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
 
-const stock_router = require('./routes/stock')
 
-app.use('/api/stock', stock_router)
+// access port values
+const PORT = process.env.PORT || 8080
+
+db_connection()
 
 app.listen(PORT, ()=> {
     console.log(`Backend server running in port ${PORT}`)
