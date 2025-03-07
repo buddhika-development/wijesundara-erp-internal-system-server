711abe744bb6fd803efdd9e4ba53f9187b3eabc8
Buddhika Madusanka <buddhikadevelopment@gmail.com>
2025-03-07 10:05:23 +0530

    Setup basic server, database connection and first sample router

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const db_connection = require('./utils/db')
+const stock_router = require('./routes/stock')
 
 dotenv.config()
 
 // configure basic app
 const app = express()
 app.use(express.json())
-app.use(express.urlencoded({extended : true}))
+app.use(express.urlencoded({extended : false}))
 
 
 // access port values
 const PORT = process.env.PORT || 8080
+
+// setup api routers
+app.use('/api/stock', stock_router)
 
 db_connection()
 
