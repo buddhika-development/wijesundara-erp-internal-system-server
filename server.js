0969fc3eca50c12086bedf894bd6078260354795
itzMeTiru <Tishanyasiru@outlook.com>
2025-03-12 13:56:04 +0530

    chore: modify HRDepartment model and server.js

--- server.js
+++ server.js
@@ -1,24 +1,31 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const db_connection = require('./utils/db')
-const stock_router = require('./routes/stock')
+const express = require('express');
+const dotenv = require('dotenv');
+const db_connection = require('./utils/db');
+const employeeRoutes = require('./routes/employeeRoutes');
+const departmentRoutes = require('./routes/departmentRoutes');
+const jobroleRoutes = require('./routes/jobroleRoutes');
+const employeeDepartmentRoutes = require('./routes/employeeDepartmentRoutes');
 
-dotenv.config()
+dotenv.config();
 
 // configure basic app
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended : false}))
-
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // access port values
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000;
 
-// setup api routers
-app.use('/api/stock', stock_router)
+// api routers
+app.use('/api/employee', employeeRoutes);
+app.use('/api/department', departmentRoutes);
+app.use('/api/jobrole', jobroleRoutes);
+app.use('/api/relation', employeeDepartmentRoutes);
 
-db_connection()
+// Database connection
+db_connection();
 
-app.listen(PORT, ()=> {
-    console.log(`Backend server running in port ${PORT}`)
-})
+// Start server
+app.listen(PORT, () => {
+    console.log(`Backend server running on port ${PORT}`);
+});
--- utils/db.js
+++ utils/db.js
@@ -3,18 +3,19 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 
-// create datanase connection
+// create database connection
 const db_connection = async() => {
 
     // access database connection string
-    const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECITON_STRING
+    const MONGO_CONNECTION_STRING = "mongodb+srv://senevirathnaanuda:finance@cluster0.gzql2.mongodb.net/HRdepartmentDB?retryWrites=true&w=majority&appName=Cluster0";
+
 
     if(MONGO_CONNECTION_STRING != null) {
 
         try{
             mongoose.connect(MONGO_CONNECTION_STRING)
 
-            console.log("Successfully connected")
+            console.log("DB Successfully connected")
         }
         catch(err) {
             console.log("Something went wrong..")
@@ -26,6 +27,5 @@ const db_connection = async() => {
     }
     
 } 
-
 
 module.exports = db_connection
