f9e663ff248aeda66f44a91a2d8c68fcf995d936
ANUDA <senevirathnaanuda@gmail.com>
2025-04-28 09:57:03 +0530

    fixed conflict errors

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const cors = require('cors'); 
+const cors = require('cors');
 const db_connection = require('./utils/db');
 const finance_router = require('./routes/finance');
 
 dotenv.config();
 
+// configure basic app
 const app = express();
-
-
-app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
-const PORT = process.env.PORT || 8080;
+// access port values
+const PORT = process.env.PORT || 5000;
 
-db_connection().then(() => {
-    console.log("MongoDB connection established");
-    app.use('/', finance_router);
+// api routers
+app.use('/', finance_router);
 
-    app.listen(PORT, () => {
-        console.log(`Backend server running on port ${PORT}`);
-    });
-}).catch(err => {
-    console.error("Failed to start the server due to DB error:", err);
+// Database connection
+db_connection();
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Backend server running on port ${PORT}`);
 });
