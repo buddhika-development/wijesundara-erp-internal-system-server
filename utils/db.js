4ebd54995c6246a8e769e47763a924c588906edd
itzMeTiru <Tishanyasiru@outlook.com>
2025-05-02 14:56:42 +0530

    fixed .env error
    added connection string

--- utils/db.js
+++ utils/db.js
@@ -7,8 +7,7 @@ dotenv.config()
 const db_connection = async() => {
 
     // access database connection string
-    const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECITON_STRING;
-
+    const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb+srv://senevirathnaanuda:finance@cluster0.gzql2.mongodb.net/HRdepartmentDB?retryWrites=true&w=majority&appName=Cluster0" ;
 
     if(MONGO_CONNECTION_STRING != null) {
 
